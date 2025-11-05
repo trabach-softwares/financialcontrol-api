77474d6e5c329f09cdf498dce0fb4f7d1cebbda4
Jonathan-trabach <trabachjonathan@gmail.com>
2025-11-05 15:43:40 -0300

    add route helth

--- src/app.js
+++ src/app.js
@@ -70,7 +70,7 @@ app.get('/', (req, res) => {
   });
 });
 
-app.get('/health', (req, res) => {
+app.get('/api/health', (req, res) => {
   res.json({
     success: true,
     message: 'API is healthy',
